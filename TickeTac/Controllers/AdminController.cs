61a22721e30f7c26d6c0c2c3f6a6fb230a033ce8
Leonardo Alves <leonardoalves72@hotmail.com>
2023-06-21 22:20:56 -0300

    Admin index Changes

--- TickeTac/Controllers/AdminController.cs
+++ TickeTac/Controllers/AdminController.cs
@@ -1,11 +1,11 @@
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using TickeTac.Models;
+using TickeTac.Data;
+using TickeTac.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TickeTac.Controllers
 {  
@@ -13,15 +13,25 @@ namespace TickeTac.Controllers
     public class AdminController : Controller
     {
         private readonly ILogger<AdminController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public AdminController(ILogger<AdminController> logger)
+        public AdminController(ILogger<AdminController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel hvm = new()
+        {
+            Categories = _context.Categories.ToList(),
+            Events = _context.Events.ToList(),
+            Cities = _context.Cities.ToList(),
+            Users = _context.AppUsers.ToList(),
+            StatusEvents = _context.StatusEvents.ToList()
+        };
+            return View(hvm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
